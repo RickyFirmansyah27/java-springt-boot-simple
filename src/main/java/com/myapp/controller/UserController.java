ddeb50179c04aa23e8b95a2f6f9cef1c5f2985df
Ricky Firmansyah <rickyfirmansyah2707@gmail.com>
2024-09-29 18:21:44 +0700

    add base response json

--- src/main/java/com/myapp/controller/UserController.java
+++ src/main/java/com/myapp/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.myapp.controller;
 
+import com.myapp.model.Entity.User;
+import com.myapp.response.BaseResponse;
+import com.myapp.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import com.myapp.model.Entity.User;
-import com.myapp.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -12,16 +15,22 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @GetMapping
-    public List<User> getUsers() {
-        return userService.getAllUsers();
+    public BaseResponse<List<User>> getUsers() {
+        logger.info("Fetching all users");
+        List<User> users = userService.getAllUsers();
+        return new BaseResponse<>("success", "Users fetched successfully", users);
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userService.createUser(user);
+    public BaseResponse<User> createUser(@RequestBody User user) {
+        logger.info("Creating a new user with name: {}", user.getName());
+        User createdUser = userService.createUser(user);
+        return new BaseResponse<>("success", "User created successfully", createdUser);
     }
 }
